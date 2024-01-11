292ff5bbe798567ab301b68545f2ae47ada8847b
justin <pulchritude007@163.com>
2024-01-11 11:13:16 +0800

    fix

--- tokio-my-redis/src/main.rs
+++ tokio-my-redis/src/main.rs
@@ -1,10 +1,14 @@
 use crate::bytes::{bytes, bytes_bufmut, cursor};
+use crate::mini_tokio::mini_tokio;
 
 mod examples;
 mod bytes;
+mod mini_tokio;
 
 fn main() {
     bytes();
     bytes_bufmut();
     cursor();
+
+    mini_tokio();
 }
