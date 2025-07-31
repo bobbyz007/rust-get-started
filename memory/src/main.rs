7701bf4b8eb993b66df88b986d5e30783c0e73d6
justin <pulchritude007@163.com>
2025-07-31 19:19:52 +0800

    fix

--- memory/src/main.rs
+++ memory/src/main.rs
@@ -1,9 +1,11 @@
 use utils::println_format_marker;
 use crate::align::{mem_size, mem_size_composite};
 use crate::pointer::{smart_pointer, smart_pointer_destruct, smart_pointer_shadowing};
+use crate::reference_cycle::reference_cycle_test;
 
 mod align;
 mod pointer;
+mod reference_cycle;
 
 fn main() {
     println_format_marker("alignment");
@@ -14,4 +16,6 @@ fn main() {
     smart_pointer();
     smart_pointer_shadowing();
     smart_pointer_destruct();
+
+    reference_cycle_test();
 }
